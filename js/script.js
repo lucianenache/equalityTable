7e982df076b5b4cf08cb798acfb757ada5df233c
Lukas Ames <lukas.ames@zanox.com>
2014-04-04 17:27:13 +0200

    integrated JSDoc and some comments for testing

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
+/**
+* @fileOverview
+* Angular App
+* @author Lucian Enache - Lucian.Enache@zanox.com 
+* @author Lukas Ames - Lukas.Ames@zanox.com 
+* @version 0.1
+*
+* Also, check out {@link http://www.google.com|Google} and
+* {@link https://github.com GitHub}.
+*/
+
+
+
+
+
 var app = angular.module('box',[]);
+
+/**
+ * Controller.
+ * @constructor
+ * @param boxCtrl
+ */
 
 app.controller('boxCtrl',function($scope){
   
@@ -10,6 +31,7 @@ app.controller('boxCtrl',function($scope){
   
   
   $scope.descr=["true","false","\"true\"","\"false\"","1","-1","1/0","-1/0","undef.","0","null","a","\"\"","[]","{}","[[]]","[0]","[1]","[-1]","NaN","[NaN]"];
+  
   
   $scope.draw = function(num){
    $scope.operator=num;
